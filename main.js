63f892b37ec24332739d71e749dfb76c8f6bbd07
Alex Wildman <Alex>
2017-10-27 14:18:57 -0600

    created flags for marking

--- main.js
+++ main.js
@@ -7,14 +7,45 @@ var mainVm = new Vue({
     methods: {
         
         findlocation: function(event) {
-            console.log(event.offsetX, event.offsetY);
+            //console.log(event);
             this.dot.push ({
                 dotx: event.offsetX + 'px',
-                doty: event.offsetY + 'px'
+                doty: event.offsetY +  'px'
             })
-        } 
-
+        },
+        removemarker: function(dot, index, event) {
+            event.stopPropagation()
+            this.dot.splice(index, 1)
+        }
 
 
     }
 });
+
+
+
+// var mainVM = new Vue({
+//    el: "#app",
+//    data: {
+//        markers: [] //`<img class="marker" src="Micky_Mouse">`
+//    },
+//    methods: {
+//        makeAMouse: function($event) {
+//            //create marker at x and y
+//            this.markers.push({
+//                pageX: event.offsetX-10 + 'px',
+//                pageY: event.offsetY-30 + 'px',
+//            })
+//            // return this.marker
+//            // console.log($event)
+//            // $event.pageX
+//            // $event.page
+//            // $event.bubbles: false
+//        },
+//        mouseTrap: function(mouse, index, event) {
+//            event.stopPropagation()
+//            this.markers.splice(index, 1)
+
+//        }
+//    }
+// })
